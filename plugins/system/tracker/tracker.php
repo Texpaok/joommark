508a2fcc1e3524e71e1bc4b498f75abea7044c69
Texpaok <texpaok@gmail.com>
2015-09-09 19:31:14 +0200

    cover

---
+++ plugins/system/tracker/tracker.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* @ author Jose A. Luque
+* @ Copyright (c) 2011 - Jose A. Luque
+* @license GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+*/
+defined( '_JEXEC' ) or die( 'Restricted access' );
+jimport( 'joomla.plugin.plugin' );
+
+class plgSystemTracker extends JPlugin{
+	
+	
+	function plgSystemTracker( &$subject, $config ){
+		parent::__construct( $subject, $config );		
+			
+	}
+	
+	
+	
+	function onAfterInitialise(){
+	
+						
+	
+
+	}
+	
+	
+	
+		
+}
